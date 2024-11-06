d3dc7da28e46b60244098a16e67cc58345a8b9f1
Sebastian Levi <sebastianLevi84@gmail.com>
2024-11-06 17:57:02 +0100

    function for the font, for h2 tag

---
+++ script.js
@@ -0,0 +1,10 @@
+const titleTwo = document.querySelector('.titleTwo');
+const titleThree = document.querySelector('.titleThree')
+
+function changeTitleTwo() {
+    titleTwo.style.fontFamily  = 'Libre Caslon Text';
+    titleTwo.style.fontSize  = '3rem';
+    titleTwo.style.lineHeight = '4rem';
+}
+
+changeTitleTwo();
